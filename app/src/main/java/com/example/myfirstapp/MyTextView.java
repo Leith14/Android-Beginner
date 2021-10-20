58da1c353b197274dec8647719252ff80ad68d95
Xuewen.Lei <Xuewen.Lei@ff.com>
2021-10-20 15:00:14 +0800

    textview03_1020 marquee text

---
+++ app/src/main/java/com/example/myfirstapp/MyTextView.java
@@ -0,0 +1,33 @@
+package com.example.myfirstapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyTextView extends TextView {
+
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
